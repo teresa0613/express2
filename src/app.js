5ce1b3e1b6a245dba7ab2ef0a7b60413da5463c4
teresa0613 <teresa1306@hotmail.com>
2020-10-01 22:06:46 +0200

    message middleware added

--- src/app.js
+++ src/app.js
@@ -1,8 +1,3 @@
-//Create an program app.js that uses express.
-//This app will need to run on your local ip and port 7777.
-//With your browser by connecting in http to your local ip on the port 7777you will have to retrieve the message
-//' Express exercises part 2 ' For this you will need to create a route that manages the path /
-
 import express from 'express'
 const app = express()
 
@@ -10,18 +5,49 @@ const IP_LOOPBACK = 'localhost'
 const IP_LOCAL = '192.168.1.126' // my local ip on my network
 const PORT = 7777
 
-const date = Date()
+//Add a middleware, /sendMessagewhich will apply to all the routes
+//and which will take care of sending the response message to the user.
+//There will be changes to apply to the routes you wrote earlier, so that they can handle this middleware.
 
-app.get('/', (req, res) => {
+//Message Middleware
+app.use('/send_message', function (req, res, next) {
+    console.log('your message: ')
+    next()
+})
+
+//' Express exercises part 2 ' For this you will need to create a route that manages the path /
+
+app.get('/send_message/home', (req, res) => {
     res.send('Express exercises part 2')
 })
 
 //Add a route /get_current_time that will return the date of the server on which running to the app.js is user
 
-app.get('/get_current_time', (req, res) => {
+app.get('/send_message/get_current_time', (req, res) => {
     const date = Date()
     res.send(`${date}`)
 })
+
+// Add a route /how_pass_data that returns a message to the user that explains
+//how we pass data from one handler / middleware to another on express
+
+app.get('/send_message/how_pass_data', (req, res) => {
+    res.send('Handled with app.use')
+})
+
+/*
+<!DOCTYPE html > 
+< html  lang =" en " > 
+  < head > 
+    < meta  charset =" utf-8 " />
+     < title > Exercices express 2 </ title > 
+  </ head > 
+
+  < body > 
+    < p > LE TEXTE A RETOURNER DEVRA SE TROUVER ICI </ p > 
+  </ body > 
+</ html > 
+*/
 
 // start the server
 app.listen(PORT, IP_LOCAL, () => {
